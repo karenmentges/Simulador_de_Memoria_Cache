5fa22bd991e5fcfa5a0c05d942ca412b818e98bb
karenmentges <karen_ruvermentges@hotmail.com>
2021-05-22 15:00:29 -0300

    Arrumando impressões em hexadecimal

--- Cache.c
+++ Cache.c
@@ -30,16 +30,20 @@ int main(){
     inicializaMemoria(BlocosMP, 32);
     inicializaMemoria(LinhasCache, 8);
 
+    printf("\n");
+    printf("Memória Principal:\n\n");
     printf("Bloco    Validade    Rotulo    Célula 0    Célula 1    Célula 2    Célula 3\n");
     for(int j=0; j<32; j++){
-        printf("%d    %d    %d    %d    %d    %d    %d", j, BlocosMP[j].validade, BlocosMP[j].rotulo, BlocosMP[j].celula[0], BlocosMP[j].celula[1], BlocosMP[j].celula[2], BlocosMP[j].celula[3]);
+        printf("0x%02x         %d        0x%02x       0x%02x        0x%02x        0x%02x        0x%02x", j, BlocosMP[j].validade, BlocosMP[j].rotulo, BlocosMP[j].celula[0], BlocosMP[j].celula[1], BlocosMP[j].celula[2], BlocosMP[j].celula[3]);
         printf("\n");
     }
     printf("\n");
 
+    printf("\n");
+    printf("Cache:\n\n");
     printf("Validade    Rotulo    Célula 0    Célula 1    Célula 2    Célula 3\n");
     for(int j=0; j<8; j++){
-        printf("%d    %d    %d    %d    %d    %d", LinhasCache[j].validade, LinhasCache[j].rotulo, LinhasCache[j].celula[0], LinhasCache[j].celula[1], LinhasCache[j].celula[2], LinhasCache[j].celula[3]);
+        printf("    %d        0x%02x       0x%02x        0x%02x        0x%02x        0x%02x", LinhasCache[j].validade, LinhasCache[j].rotulo, LinhasCache[j].celula[0], LinhasCache[j].celula[1], LinhasCache[j].celula[2], LinhasCache[j].celula[3]);
         printf("\n");
     }
     printf("\n");
